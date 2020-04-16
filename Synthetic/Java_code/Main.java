b0759b3d5ca9042a1db8afd58dd885ab313f623b
George <gwisaacs2@gmail.com>
2020-04-15 20:03:26 -0400

    Now getting depths from pixel value

--- Synthetic/Java_code/Main.java
+++ Synthetic/Java_code/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 import java.util.ArrayList;
 import java.io.FileReader;
 import java.io.IOException;
@@ -6,6 +7,14 @@ import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+
 
 
 public class Main
@@ -23,13 +32,75 @@ public class Main
         }
         double radians = Math.toRadians(90 - heading); //converts heading (compass) to trig. longitude is x axis and lat is y axis
 
+        final String image_location = "../Pictures/Puerto_Rico_colored.jpg";
+
         ArrayList<Waypoints> waypoints = new ArrayList<Waypoints>(); //array containing our waypoints
-        waypoints = insertWaypoints( waypoints, input + ".txt" ); //insert the first run
-        waypoints = findPath(waypoints, radians);
-        outputWaypoints(waypoints, output + ".txt");
+
+        waypoints = insertWaypoints( waypoints, "../Inputs/" + input + ".txt" ); //insert the first run
+        waypoints = findPath(waypoints, radians); // get waypoints for the rest of the path
+
+        getDepths(waypoints, image_location); // find the depths at each waypoint
+//         markObstacles(waypoints); // outputs file with flags at the obstacles
+
+        outputWaypoints(waypoints, "../Outputs/" + output + ".txt"); //output the track of the whole path
+//         outputNMEA(waypoints);
+
 
     }
 
+    public static void getDepths(ArrayList<Waypoints> waypoints, String image_location)
+    {
+        final int MAX_DEPTH = 1000;
+        final int MIN_DEPTH = 0;
+        final int MAX_HUE = 240;
+        final int MIN_HUE = 0;
+
+        final double LAT_MIN = 18.426;
+        final double LAT_MAX = 18.740;
+        final double LON_MIN = -66.50;
+        final double LON_MAX = -66.00;
+
+        System.out.println("Scanning the water for debris...");
+        System.out.println("Reading from " + image_location);
+
+        BufferedImage image;
+        int height, width;
+        double lat, lon;
+        try
+        {
+            File file = new File(image_location);
+            if (!file.canRead())
+                System.out.println("Cannot read");
+            FileInputStream fis = new FileInputStream(file);
+            image = ImageIO.read(fis);
+
+            width = image.getWidth();
+            height = image.getHeight();
+            System.out.printf("\n---------------\nHeight: %d\nWidth: %d\n------------\n", height, width);
+
+            int i, j;
+            float hue;
+            float [] hsb;
+            double depth, ratio;
+            for (Waypoints item: waypoints)
+            {
+                lat = item.getLattitude();
+                lon = item.getLongitude();
+                i = (int) Math.floor( Math.abs( (lat - LAT_MAX) / (LAT_MAX - LAT_MIN) * height) ); // subtracting from lat max because coordinates of the pixels are row & column, but the lat max is at the top of the picture
+                j = (int) Math.floor( Math.abs( (lon - LON_MIN) / (LON_MAX - LON_MIN) * width) );
+                Color c = new Color(image.getRGB(j,i));
+                hsb = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
+                hue = hsb[0];
+                ratio = ( (hue * 360) - MIN_HUE ) / (MAX_HUE - MIN_HUE);
+                depth = ratio * (MAX_DEPTH - MIN_DEPTH) + MIN_DEPTH;
+
+                item.setDepth( (int) depth);
+                System.out.printf("\nAt %d row and %d column, the depth is %d\n", i, j, (int) depth);
+            }
+        } catch (Exception e) {}
+
+
+    }
     public static ArrayList<Waypoints> findPath(ArrayList<Waypoints> waypoints, double radians)
     {
         System.out.println("Finding the optimal path...");
--- Synthetic/run.sh
+++ Synthetic/run.sh
@@ -25,3 +25,13 @@ input="../Inputs/input"
 output="../Outputs/output"
 heading=360
 java Main $input $output $heading
+
+
+cd "../C_code"
+lat_min=30
+lat_max=30
+lon_min=30
+lon_max=30
+pixels_W=1149
+pixels_H=751
+./a.out $lat_min $lat_max $lon_min $lon_max $pixels_W $pixels_H
